7319ac203fc60c4697a04df373db16ca5e84562e
OlgaMykhailova <rosebud_z@yahoo.com>
2023-02-02 23:08:32 +0200

    Update addContact.js

--- controllers/contacts/addContact.js
+++ controllers/contacts/addContact.js
@@ -1,8 +1,16 @@
 const { Contact } = require("../../models/contact");
+const createError = require("http-errors");
 
 const addContact = async (req, res, next) => {
   const { _id } = req.user;
   const { name, number } = req.body;
+  const candidate = Contact.findOne({ number, owner: _id });
+  if (candidate) {
+    throw createError(
+      409,
+      `Contact with number ${number} is already in your contact book`
+    );
+  }
   const data = await Contact.create({ name, number, owner: _id });
   res.status(201).send(data);
 };
